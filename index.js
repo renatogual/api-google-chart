a466825add698b9452b5fad42d8da2eb59970df1
eox-tecnologia <eox-tecnologia>
2020-12-10 15:59:32 -0300

    Criando lógica de construção dos gráficos

--- index.js
+++ index.js
@@ -1,82 +1,62 @@
+const dadosAtivas = [];
+const dadosReceptivas = [];
+const dadosInternas = [];
+
 // Load the Visualization API and the corechart package.
 google.charts.load('current', {'packages':['corechart'], 'language': 'pt-br'});
 
 // Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawChartPie);
-google.charts.setOnLoadCallback(drawChartColumn);
-google.charts.setOnLoadCallback(drawChartLine);
+google.charts.setOnLoadCallback(drawChartActives);
 
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
 // draws it.
-async function drawChartPie() {
-  let dados = await getDados();
+async function drawChartActives() {
+  let dados = dadosAtivas.flat().map(item => item.slice(1,3))
+
   // Create the data table.
   let data = new google.visualization.DataTable();
   data.addColumn('string');
   data.addColumn('number');
-  data.addRows(dados.Dados);
+  data.addRows(dados);
 
-  // Set chart options
-  let options = {
-    'title':'Gráfico de teste',
-    // 'is3D': true,
-    'legend': {},
+  // Set Pie chart options
+  let piechart_options = {
+    'title':'Ligações Ativas',
+    'is3D': true,
     'width':900,
     'height':500,
   };
 
-  // Instantiate and draw our chart, passing in some options.
-  let chart = new google.visualization.PieChart(document.getElementById('chart_pie'));
-  chart.draw(data, options);
-}
-
-async function drawChartColumn() {
-  let dados = await getDados();
-  // Create the data table.
-  let data = new google.visualization.DataTable();
-  data.addColumn('string');
-  data.addColumn('number');
-  data.addRows(dados.Dados);
-
-  // Set chart options
-  let options = {
-    'title':'Gráfico de teste',
+  // Instantiate and draw our Pie chart, passing in some options.
+  let piechart_div = document.getElementById('chart_pie_actives');
+  let piechart = new google.visualization.PieChart(piechart_div);
+  piechart.draw(data, piechart_options);
+  
+  // Set Column chart options
+    let columnchart_options = {
+    'title':'Ligações Ativas',
     'width':900,
     'height':500,
   };
 
-  // Instantiate and draw our chart, passing in some options.
-  let chart = new google.visualization.ColumnChart(document.getElementById('chart_column'));
-  chart.draw(data, options);
-}
-
-async function drawChartLine() {
-  let dados = await getDados();
-  // Create the data table.
-  let data = new google.visualization.DataTable();
-  data.addColumn('string');
-  data.addColumn('number');
-  data.addRows(dados.Dados);
-
-  // Set chart options
-  let options = {
-    'title':'Gráfico de teste',
-    'width':900,
-    'height':500,
-  };
-
-  // Instantiate and draw our chart, passing in some options.
-  let chart = new google.visualization.LineChart(document.getElementById('chart_line'));
-  chart.draw(data, options);
+  // Instantiate and draw our Column chart, passing in some options.
+  let columnchart_div = document.getElementById('chart_column_actives');
+  let columnchart = new google.visualization.ColumnChart(columnchart_div);
+  columnchart.draw(data, columnchart_options);
 }
 
 async function getDados() {
   let response = await fetch(`./dados.json`);
   let data = await response.json();
-  return data
+  
+  dadosAtivas.push(data.Dados.filter(itens => itens[0].toLowerCase() === 'ativa'));
+  dadosReceptivas.push(data.Dados.filter(itens => itens[0].toLowerCase() === 'receptiva'));
+  dadosInternas.push(data.Dados.filter(itens => itens[0].toLowerCase() === 'interna'));
 }
 
 $(document).ready(function(){
   $('.tabs').tabs();
+  
+  getDados();
 });
