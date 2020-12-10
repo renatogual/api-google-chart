7e8449080aa22a6ecba3b570983fed75ca246f95
eox-tecnologia <eox-tecnologia>
2020-12-10 17:40:49 -0300

    Organizando melhor as funções de construção dos gráficos

--- index.js
+++ index.js
@@ -2,50 +2,6 @@ const dadosAtivas = [];
 const dadosReceptivas = [];
 const dadosInternas = [];
 
-// Load the Visualization API and the corechart package.
-google.charts.load('current', {'packages':['corechart'], 'language': 'pt-br'});
-
-// Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawChartActives);
-
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
-async function drawChartActives() {
-  let dados = dadosAtivas.flat().map(item => item.slice(1,3))
-
-  // Create the data table.
-  let data = new google.visualization.DataTable();
-  data.addColumn('string');
-  data.addColumn('number');
-  data.addRows(dados);
-
-  // Set Pie chart options
-  let piechart_options = {
-    'title':'Ligações Ativas',
-    'is3D': true,
-    'width':900,
-    'height':500,
-  };
-
-  // Instantiate and draw our Pie chart, passing in some options.
-  let piechart_div = document.getElementById('chart_pie_actives');
-  let piechart = new google.visualization.PieChart(piechart_div);
-  piechart.draw(data, piechart_options);
-  
-  // Set Column chart options
-    let columnchart_options = {
-    'title':'Ligações Ativas',
-    'width':900,
-    'height':500,
-  };
-
-  // Instantiate and draw our Column chart, passing in some options.
-  let columnchart_div = document.getElementById('chart_column_actives');
-  let columnchart = new google.visualization.ColumnChart(columnchart_div);
-  columnchart.draw(data, columnchart_options);
-}
-
 async function getDados() {
   let response = await fetch(`./dados.json`);
   let data = await response.json();
@@ -55,8 +11,129 @@ async function getDados() {
   dadosInternas.push(data.Dados.filter(itens => itens[0].toLowerCase() === 'interna'));
 }
 
+function dataTable(tipo) {
+  let dataFormated = tipo.flat().map(item => item.slice(1,3));
+
+  // Create the data table.
+  let dataTable = new google.visualization.DataTable();
+  dataTable.addColumn('string');
+  dataTable.addColumn('number');
+  dataTable.addRows(dataFormated);
+
+  return dataTable
+}
+
+function pieChartActives(data) {
+  // Set Pie chart options
+  let piechart_options = {
+    'title': 'Ligações Ativas',
+    'is3D': true,
+    'width':900,
+    'height':500,
+  };
+
+  // Instantiate and draw our Pie chart, passing in some options.
+  let piechart_div_actives = document.getElementById('chart_pie_actives');
+  let piechart_actives = new google.visualization.PieChart(piechart_div_actives);
+  piechart_actives.draw(data, piechart_options);
+}
+
+function pieChartReceptives(data) {
+  // Set Pie chart options
+  let piechart_options = {
+    'title': 'Ligações Receptivas',
+    'is3D': true,
+    'width':900,
+    'height':500,
+  };
+
+  let piechart_div_receptives = document.getElementById('chart_pie_receptives');
+  let piechart_receptives = new google.visualization.PieChart(piechart_div_receptives);
+  piechart_receptives.draw(data, piechart_options);
+}
+
+function pieChartInternals(data) {
+  // Set Pie chart options
+  let piechart_options = {
+    'title': 'Ligações Internas',
+    'is3D': true,
+    'width':900,
+    'height':500,
+  };
+
+  let piechart_div_internals = document.getElementById('chart_pie_internals');
+  let piechart_internals = new google.visualization.PieChart(piechart_div_internals);
+  piechart_internals.draw(data, piechart_options);
+}
+
+function columnChartActives(data) {
+  // Set Column chart options
+  let columnchart_options = {
+    'width':900,
+    'height':500,
+  };
+
+  // Instantiate and draw our Pie chart, passing in some options.
+  let columnchart_div_actives = document.getElementById('chart_column_actives');
+  let columnchart_actives = new google.visualization.ColumnChart(columnchart_div_actives);
+  columnchart_actives.draw(data, columnchart_options);
+}
+
+function columnChartReceptives(data) {
+  // Set Column chart options
+  let columnchart_options = {
+    'width':900,
+    'height':500,
+  };
+
+  let columnchart_div_receptives = document.getElementById('chart_column_receptives');
+  let columnchart_receptives = new google.visualization.ColumnChart(columnchart_div_receptives);
+  columnchart_receptives.draw(data, columnchart_options);
+}
+
+function columnChartInternals(data) {
+  // Set Column chart options
+  let columnchart_options = {
+    'width':900,
+    'height':500,
+  };
+
+  let columnchart_div_internals = document.getElementById('chart_column_internals');
+  let columnchart_internals = new google.visualization.ColumnChart(columnchart_div_internals);
+  columnchart_internals.draw(data, columnchart_options);
+}
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChartActives() {
+  let data = dataTable(dadosAtivas);
+  pieChartActives(data);
+  columnChartActives(data);
+}
+
+function drawChartReceptives() {
+  let data = dataTable(dadosReceptivas);
+  pieChartReceptives(data);
+  columnChartReceptives(data);
+}
+
+function drawChartInternals() {
+  let data = dataTable(dadosInternas);
+  pieChartInternals(data);
+  columnChartInternals(data);
+}
+
 $(document).ready(function(){
   $('.tabs').tabs();
   
   getDados();
+
+  // Load the Visualization API and the corechart package.
+  google.charts.load('current', {'packages':['corechart'], 'language': 'pt-br'});
+
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.charts.setOnLoadCallback(drawChartActives);
+  google.charts.setOnLoadCallback(drawChartReceptives);
+  google.charts.setOnLoadCallback(drawChartInternals);
 });
